45a386d0f9ac0430e451c7240982d0571bd66abb
BolivianProgrammer <tylerj@tylerlabs.com>
2025-09-18 19:33:04 -0400

    Configuracion base de datos

---
+++ PedidosApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PedidosApp.Models;
+
+namespace PedidosApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define your DbSet properties for your entities here
+        // For example:
+        // public DbSet<Pedido> Pedidos { get; set; }
+        // public DbSet<Cliente> Clientes { get; set; }
+    }
+}
